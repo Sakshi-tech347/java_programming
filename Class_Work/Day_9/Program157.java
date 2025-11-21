ba5945a4d20a340b4a8fcae71f98709c5d30a24f
sakshi kachate <sakshikachate347@gmail.com>
2025-11-21 13:31:42 +0530

    print -6 -5 -4 -3 -2 -1

---
+++ Class_Work/Day_9/Program157.java
@@ -0,0 +1,28 @@
+
+import java.util.*;
+
+class Pattern
+{
+    public void Display(int iNo)
+    {
+        int iCnt =0;
+        for(iCnt = -iNo ; iCnt <= -1 ; iCnt ++ )
+        {
+            System.out.print(iCnt+"\t");
+        }
+    }
+}
+class Program157
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+        int iValue = 0;
+
+        System.out.println("Enter the number of Frequency:");
+        iValue = sobj.nextInt();
+
+        Pattern pobj = new Pattern();
+        pobj.Display(iValue);
+    }
+}
